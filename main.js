37450f9512b2566db547c1541a8d11dfff41a3f7
Fereshteh Forghani <fereshtforghani@gmail.com>
2025-04-04 21:31:05 -0400

    SFC visualizations added

--- main.js
+++ main.js
@@ -3,6 +3,10 @@
 var scenes = [
 	'05ce34e3cd48c449', '02e6fb86b0172f0b', '03b3f603a1001de0','06a8196a66e125af','02b2358ff02d3ce8', '0090cc64d7b7bb24','00ca5123d8ff6f83'
 ];
+var sfc_scenes = [
+	'02e6fb86b0172f0b', '05ce34e3cd48c449', '02b2358ff02d3ce8', '000db54a47bd43fe', '000c3ab189999a83', '00ca5123d8ff6f83', '00cfc0ecd345deb4'
+]
+
 var roi = {
 	'0090cc64d7b7bb24':[{'bx':95,'by':35,'bh':40,'bw':80},{'bx':70,'by':180,'bh':150,'bw':80}, {'bx':220,'by':100,'bh':50,'bw':80}],
 	'00ca5123d8ff6f83':[{'bx':80,'by':150,'bh':200,'bw':40}],
@@ -150,6 +154,112 @@ class Sample_viewer{
 	}
 };
 
+class Sample_viewer_sfc{
+	/*
+	Viewer setup needs a mix of HTML and JS
+	See the HTML and this class to see how to structure the HTML elements, their ids, and JS callbacks
+	The prefix argument i used to identify the viewer, needs to be consistent with HTML for the JS to find the right elements
+	*/
+	constructor(prefix,max_idx,scene_codes){
+		this.scene_codes = scene_codes
+		this.n_scenes = scene_codes.length;
+		this.prefix = prefix;
+		this.max_idx = max_idx;
+		this.cur_frame = 0;
+		this.cur_sample = 0;
+		this.scene_code = scene_codes[0];
+		this.need_stop_anim = false;
+		this.interval_id = null;
+		this.anim_dir = 1;
+		this.showing_box = false;
+		for (let i=0;i<this.n_scenes;i++){
+			document.getElementById(`${this.prefix}-scene-selector`).innerHTML += `<div onclick="${this.prefix}_viewer.change_scene(\'${scene_codes[i]}\');" class="col-1"> <img style="border-radius:1em;" class=selectable src="assets/sfc/cond/${scene_codes[i]}.png"> </div>`;
+		}
+	}
+	update_ims(){
+		/*
+		This is the main method that takes all the image parameters and updates the images in the web page
+		*/
+		let frame_padded = this.cur_frame.toString();
+		document.getElementById(`${this.prefix}-im`).src = `assets/sfc/frames/${this.scene_code}/${frame_padded}.png`;
+	}
+
+	/* ===================================================================================
+	The methods below are used for image control, called by pushing buttons on the HTML
+	=================================================================================== */
+	change_scene(scene_code){
+		this.scene_code = scene_code;
+		this.update_ims();
+		this.toggle_box();
+		this.toggle_box();
+	}
+	change_variant(name){
+		this.variant = name;
+		if (this.variants){
+			for (let nn of this.variants){
+				document.getElementById(`${nn}_selector`).style.backgroundColor = '';
+				document.getElementById(`${nn}_selector`).style.borderRadius = '1em';
+			}
+			document.getElementById(`${name}_selector`).style.backgroundColor = 'lightgrey';
+			document.getElementById(`${name}_selector`).style.borderRadius = '1em';
+		}
+		this.update_ims();
+	}
+
+	/* ===================================================================================
+	The methods below are used for automatic playback
+	=================================================================================== */
+	change_frame(idx){
+		/*
+		This is called when the user clicks and drags on the slider to see a specific frame
+		This also stops the automatic playback
+		*/
+		this.stop_anim();
+		this.cur_frame = parseInt(idx);
+		this.update_ims();
+	}
+	next_frame(){
+		/*
+		This is used internally to play the sequence forward and backward, and also moves the slider to show the user what frame is being shown
+		*/
+		this.cur_frame += this.anim_dir;
+		if (this.cur_frame >= this.max_idx) {this.anim_dir=-1;}
+		if (this.cur_frame <= 0) {this.anim_dir=1;}
+		document.getElementById(`${this.prefix}_frame_control`).value = this.cur_frame;
+		this.update_ims();
+	}
+	cycle_frames(delay){
+		/*
+		Starts the automatic playback using JS intervals, see next_frame() to see the cycling behavior
+		*/
+		this.stop_anim();
+		this.interval_id = setInterval(function() {
+			this.next_frame();
+		}.bind(this), delay);
+		this.update_ims();
+	}
+	stop_anim(){
+		if (this.interval_id){clearInterval(this.interval_id);}
+		this.interval_id = null;
+	}
+	toggle_box(){
+		if (this.showing_box){
+			this.clear_box();
+			this.showing_box = false;
+		}
+		else{
+			this.place_box();
+			this.showing_box = true;
+		}
+	}
+	clear_box(){
+		const svg = document.getElementById("box_svg");
+		while (svg.firstChild) {
+				svg.removeChild(svg.firstChild); // Removes all child elements
+		}
+	}
+};
+
 // create the viewer here to make it global, and accessible from the HTML
 var edge_viewer = null;
 
@@ -158,4 +268,8 @@ document.addEventListener("DOMContentLoaded", function() {
 	edge_viewer = new Sample_viewer('edge',9,scenes);
 	edge_viewer.change_frame(0);
 	edge_viewer.cycle_frames(100);
+
+	sfc_viewer = new Sample_viewer_sfc('sfc',9,sfc_scenes);
+	sfc_viewer.change_frame(0);
+	sfc_viewer.cycle_frames(100);
 });
